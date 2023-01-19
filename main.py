fe2c177c1a644a270d8373add29f8ff9601d1621
joao0812 <joaoesalmeida@hotmail.com>
2023-01-19 09:38:56 -0300

    Implementa API com Flask

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ cursor.execute(
 # cursor.execute("INSERT INTO videos VALUES(3, 'videoF', 200, 2)")
 # banco.commit()
 
+
 def get_columns():
     c = sqlite3.connect('videos.db').cursor()
     all_id = c.execute("SELECT id FROM videos").fetchall()
@@ -23,13 +24,31 @@ def get_columns():
 # desc = [description[0] for description in cursor.description]
 # print(desc)
 
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument(
+    "name", type=str, help="Name of the Video is required", required=True)
+video_put_args.add_argument(
+    "likes", type=int, help="Like on the Video is required", required=True)
+video_put_args.add_argument(
+    "views", type=int, help="Views of the Video is required", required=True)
+
+
 def verify_id_doesnt_exist(video_id):
     ids = get_columns()
     if video_id not in ids:
-        abort(404, message="Video ID is not exist...")        
+        abort(404, message="Video ID is not exist...")
+
+
+def verify_id_exist(video_id):
+    ids = get_columns()
+    if video_id in ids:
+        abort(409, message="Video ID already exist...")
+
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class Video(Resource):
     def get(self, video_id):
@@ -39,6 +58,15 @@ class Video(Resource):
         c.execute(f"SELECT * FROM videos WHERE id = {video_id}")
         data = c.fetchall()
         return jsonify(data)
+
+    def put(self, video_id):
+        verify_id_exist(video_id)
+        args = video_put_args.parse_args()
+        banco = sqlite3.connect('videos.db')
+        c = banco.cursor()
+        c.execute(f"INSERT INTO videos(id, name, views, likes) VALUES(?, ?, ?, ?)", (video_id, args['name'], args['views'], args['likes']))
+        banco.commit()
+        return args
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- temp/put.py
+++ temp/put.py
@@ -4,8 +4,5 @@ BASE = "http://127.0.0.1:5000/"
 
 data = {"name": "VideoA", "views": 13, "likes": 123}
 
-response = requests.get(f'{BASE}video/1')
-print(response.json())
-input()
-response = requests.put(f'{BASE}video/5', data)
+response = requests.put(f'{BASE}video/4', data)
 print(response.json())
